121d4ec193f49858bea24cefbf9dde508a199c7c
Brian Oxley <andrew.dean+brian.oxley@thoughtworks.com>
2016-09-15 10:31:59 -0500

    Provide refactoring facade for previous call

--- print-salesmen.h
+++ print-salesmen.h
@@ -1,5 +1,7 @@
 #ifndef _PRINT_SALESMEN_H
 #define _PRINT_SALESMEN_H
 typedef int (*printf_t)(const char *format, ...);
-extern void print_salesmen(printf_t printer);
+extern void print_salesmen_with(printf_t printer);
+/* Cannot use inline declaration because of Pro*C */
+extern void print_salesmen();
 #endif
